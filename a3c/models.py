3662ddd77ffb134a22de79603e9844a18ba5d8ac
lmxx1234567 <lmxx1234567@hotmail.com>
2023-11-21 16:17:19 +0800

    feat: add a3c model

---
+++ a3c/models.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SharedStateFeatureExtractor(nn.Module):  # SSFE
+    def __init__(
+        self,
+        in_features,
+        d_model,
+        nhead,
+        num_encoder_layers,
+        num_decoder_layers,
+        dim_feedforward,
+    ):
+        super(SharedStateFeatureExtractor, self).__init__()
+        self.linear_projection = nn.Linear(in_features, d_model)
+
+        self.transformer = nn.Transformer(
+            d_model,
+            nhead,
+            num_encoder_layers,
+            num_decoder_layers,
+            dim_feedforward,
+            batch_first=True,
+        )
+
+    def forward(self, src, tgt=None):
+        src = self.linear_projection(src)  # (batch_size, seq_len, in_features)
+        if tgt is None:
+            tgt = src
+        else:
+            tgt = self.linear_projection(tgt)
+
+        output = self.transformer(src, tgt)  # (batch_size, seq_len, d_model)
+        return output
+
+    _instance = None
+
+    @classmethod
+    def get_instance(
+        cls,
+        in_features,
+        d_model,
+        nhead,
+        num_encoder_layers,
+        num_decoder_layers,
+        dim_feedforward,
+    ):
+        if cls._instance is None:
+            cls._instance = cls(
+                in_features,
+                d_model,
+                nhead,
+                num_encoder_layers,
+                num_decoder_layers,
+                dim_feedforward,
+            )
+        return cls._instance
+
+    def set_grad_requires(self, requires_grad):
+        for param in self.linear_projection.parameters():
+            param.requires_grad = requires_grad
+
+
+class TrafficScheduler(nn.Module):
+    def __init__(
+        self,
+        in_features,
+        d_model,
+        nhead,
+        num_encoder_layers,
+        num_decoder_layers,
+        dim_feedforward,
+    ):
+        super(TrafficScheduler, self).__init__()
+        self.state_feature_extractor = SharedStateFeatureExtractor.get_instance(
+            in_features,
+            d_model,
+            nhead,
+            num_encoder_layers,
+            num_decoder_layers,
+            dim_feedforward,
+        )
+
+        self.fc0 = nn.Linear(d_model, d_model // 2)
+        self.fc1 = nn.Linear(d_model // 2, 3)
+
+    def forward(self, src, tgt=None):
+        if tgt is None:
+            tgt = src
+
+        features = self.state_feature_extractor(src)  # (batch_size, seq_len, d_model)
+
+        fc_out = self.fc0(features)
+        output = self.fc1(fc_out)
+
+        output = torch.sigmoid(output)  # (batch_size, seq_len, 3)
+        return output
+
+
+class StateValueCritic(nn.Module):
+    def __init__(
+        self,
+        in_features,
+        d_model,
+        nhead,
+        num_encoder_layers,
+        num_decoder_layers,
+        dim_feedforward,
+    ):
+        super(StateValueCritic, self).__init__()
+        self.state_feature_extractor = SharedStateFeatureExtractor.get_instance(
+            in_features,
+            d_model,
+            nhead,
+            num_encoder_layers,
+            num_decoder_layers,
+            dim_feedforward,
+        )
+
+        self.fc0 = nn.Linear(d_model, d_model // 2)
+        self.fc1 = nn.Linear(d_model // 2, 1)
+
+    def forward(self, src, tgt=None):
+        if tgt is None:
+            tgt = src
+
+        features = self.state_feature_extractor(src)  # (batch_size, seq_len, d_model)
+
+        # Use max pooling to convert the transformer output to a single vector per sequence
+        seq_len = features.shape[1]
+        if torch.is_tensor(seq_len): # for tracing
+            seq_len = seq_len.item()
+        pooled_output = F.max_pool1d(
+            features.transpose(1, 2), seq_len
+        ).squeeze(
+            -1
+        )  # (batch_size, d_model)
+
+        fc_out = self.fc0(pooled_output)  # (batch_size, d_model//2)
+        output = self.fc1(fc_out)  # (batch_size, 1)
+
+        output = F.relu(output, inplace=True)  # (batch_size, 1)
+        return output
+
+
+if __name__ == "__main__":
+    # Test the model
+    trafficScheduler = TrafficScheduler(19, 256, 8, 3, 3, 2048)
+    src = torch.rand(1, 100, 19)
+    output = trafficScheduler(src)
+    print(output.shape)
+
+    stateValueCritic = StateValueCritic(19, 256, 8, 3, 3, 2048)
+    output = stateValueCritic(src)
+    print(output.shape)
+
+    # save model
+    import os
+
+    if not os.path.exists("saved_models/trafficScheduler"):
+        os.makedirs("saved_models/trafficScheduler")
+    if not os.path.exists("saved_models/stateValueCritic"):
+        os.makedirs("saved_models/stateValueCritic")
+    torch.save(
+        trafficScheduler.state_dict(), "saved_models/trafficScheduler/untrained.pt"
+    )
+    torch.save(
+        stateValueCritic.state_dict(), "saved_models/stateValueCritic/untrained.pt"
+    )
+
+    # save traced model
+    traced_script_module = torch.jit.trace(trafficScheduler, src)
+    traced_script_module.save("saved_models/trafficScheduler/untrain_traced.pt")
+    traced_script_module = torch.jit.trace(stateValueCritic, src)
+    traced_script_module.save("saved_models/stateValueCritic/untrain_traced.pt")
---
+++ a3c/training.py
@@ -0,0 +1,79 @@
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+import torch
+from torch.distributions import Categorical
+from a3c.models import TrafficScheduler, StateValueCritic
+
+
+def training():
+    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    # define model
+    traffic_scheduler = TrafficScheduler(19, 256, 4, 3, 3, 2048).to(dev)
+    state_value_critic = StateValueCritic(19, 256, 4, 3, 3, 2048).to(dev)
+    shared_state_feature_extractor = state_value_critic.state_feature_extractor
+
+    # load data
+    batch_size = 8
+    pass
+
+    # define loss function
+    criterion_critic = torch.nn.MSELoss()
+    optimizer_scheduler = torch.optim.Adam(traffic_scheduler.parameters(), lr=1e-3)
+    optimizer_critic = torch.optim.Adam(state_value_critic.parameters(), lr=1e-3)
+
+    # train loop
+    start_episode = 0
+    num_episodes = 1000
+    for episode in range(start_episode, num_episodes):
+        episode_reward = 0
+        episode_loss_scheduler = 0
+        episode_loss_critic = 0
+        num_steps = 60
+
+        for step in range(num_steps):
+             # read states. TODO: read from dataset
+            states,next_states = torch.randn(batch_size, 100, 19).to(dev),torch.randn(batch_size, 100, 19).to(dev)
+
+            # get actions
+            actions:torch.Tensor = traffic_scheduler(states) # (batch_size, 100, 3)
+            actions_dist = Categorical(actions)
+            actions = actions_dist.sample() # (batch_size, 100)
+            action_probs = actions_dist.log_prob(actions) # (batch_size, 100)
+
+            # get state value
+            values = state_value_critic(states) # (batch_size, 1)
+            next_values = state_value_critic(next_states) # (batch_size, 1)
+
+            # get reward. TODO: read from dataset
+            reward = torch.randn(batch_size, 1).to(dev)
+            episode_reward += reward.item()
+
+            # calculate loss
+            loss_critic = criterion_critic(values, reward + next_values)
+            td = reward + next_values - values # (batch_size, 1) temporal difference
+            td = td.detach()
+            loss_scheduler = -torch.mean(action_probs * td.unsqueeze(-1))
+
+            # update loss
+            episode_loss_scheduler += loss_scheduler.item()
+            episode_loss_critic += loss_critic.item()
+
+        # update model
+        optimizer_scheduler.zero_grad()
+        episode_loss_scheduler.backward()
+        optimizer_scheduler.step()
+        optimizer_critic.zero_grad()
+        episode_loss_critic.backward()
+        optimizer_critic.step()
+
+        # save model
+
+
+
+if __name__ == "__main__":
+    training()
